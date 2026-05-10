d68c82350b2f1055330f03dda6c22ebaeadcc576
LongHairedSkeleton <murilolima8103@gmail.com>
2026-05-09 21:53:50 -0300

    added a drawing function, problems, and equasions

--- Math.gd
+++ Math.gd
@@ -1,49 +1,82 @@
 extends Control
 
-
-
-func Roll():
-	randomize()
-	var Number1 = (randi() % 21 -10)
-	var Number2 = (randi() % 21 - 10)
-	var Sinais = ["+", "-"]
-	var SinalSorteado = Sinais[randi() % Sinais.size()]
-	
-	var conta_texto = str(Number1) + " " + SinalSorteado + " " + str(Number2)
-	$TextureRect/Label.text = conta_texto
-
-	if SinalSorteado == "+":
-		var resultado_final = Number1 + Number2
-		$TextureRect/Label2.text = " = " + str(resultado_final)
-
-	var resultado_final = Number1 + Number2
-		
-	var label1 = $TextureRect/Label2
-	var label2 = $TextureRect/Label3
-	var label3 = $TextureRect/Label4
-	var label4 = $TextureRect/Label5
-	randomize()
-	var rng = (randi() % 4) + 1
-	if rng == 1:
-		label1.text = str(resultado_final)
-		label2.text = str(resultado_final + (randi() % 21) - 10)
-		label3.text = str(resultado_final + (randi() % 21) - 10)
-		label4.text = str(resultado_final + (randi() % 21) - 10)
-	if rng == 2:
-		label1.text = str(resultado_final + (randi() % 21) - 10)
-		label2.text = str(resultado_final)
-		label3.text = str(resultado_final + (randi() % 21) - 10)
-		label4.text = str(resultado_final + (randi() % 21) - 10)
-	if rng == 3:
-		label1.text = str(resultado_final + (randi() % 21) - 10)
-		label2.text = str(resultado_final + (randi() % 21) - 10)
-		label3.text = str(resultado_final)
-		label4.text = str(resultado_final + (randi() % 21) - 10)
-	if rng == 4:
-		label1.text = str(resultado_final + (randi() % 21) - 10)
-		label2.text = str(resultado_final + (randi() % 21) - 10)
-		label3.text = str(resultado_final + (randi() % 21) - 10)
-		label4.text = str(resultado_final)
+var resultado_final = 0
+enum types {simple, problems}
+export (types) var problem_type = types.simple
 
 func _ready():
-	Roll()
+	randomize() # Call once at the start
+	roll()
+
+func roll():
+	var label1 = $TextureRect/Button/Label2
+	var label2 = $TextureRect/Button2/Label5
+	var label3 = $TextureRect/Button3/Label4
+	var label4 = $TextureRect/Button4/Label3
+
+	var Number1 = (randi() % 10) + 1
+	var Number2 = (randi() % 10) + 1
+	
+	if problem_type == types.simple:
+		var Sinais = ["+", "-"]
+		var SinalSorteado = Sinais[randi() % Sinais.size()]
+		
+		if SinalSorteado == "+":
+			resultado_final = Number1 + Number2
+		else:
+			resultado_final = Number1 - Number2
+		
+		$TextureRect/Label.text = str(Number1) + " " + SinalSorteado + " " + str(Number2)
+
+	elif problem_type == types.problems:
+		# Define problems with their math type
+		var problems = [
+			{"text": "Ana tinha %s moedas e perdeu %s. Quantas sobraram?", "op": "-"},
+			{"text": "João tinha %s balas e ganhou %s. Quantas tem agora?", "op": "+"}
+		]
+		
+		var choice = problems[randi() % problems.size()]
+		
+		# Perform the correct math based on the chosen story
+		if choice["op"] == "+":
+			resultado_final = Number1 + Number2
+		else:
+			# Prevent negative results: make sure Number1 is the bigger one
+			if Number1 < Number2:
+				var temp = Number1
+				Number1 = Number2
+				Number2 = temp
+			resultado_final = Number1 - Number2
+		
+		# Now apply the numbers to the template
+		$TextureRect/Label.text = choice["text"] % [str(Number1), str(Number2)]
+
+	label1.text = str(resultado_final + (randi() % 21) - 10)
+	label2.text = str(resultado_final + (randi() % 21) - 10)
+	label3.text = str(resultado_final + (randi() % 21) - 10)
+	label4.text = str(resultado_final + (randi() % 21) - 10)
+
+	# Overwrite one label with the correct answer
+	var possible_labels = [label1, label2, label3, label4]
+	var selected_to_be_right = possible_labels[randi() % possible_labels.size()]
+	selected_to_be_right.text = str(resultado_final) # Removed the " = "
+
+func check_if_correct(label):
+	# Now this will compare "5" == "5" instead of " = 5" == "0"
+	if label.text == str(resultado_final):
+		print("certo")
+	else:
+		print("errado")
+	roll()
+
+func _on_Button_pressed():
+	check_if_correct($TextureRect/Button/Label2)
+
+func _on_Button2_pressed():
+	check_if_correct($TextureRect/Button2/Label5)
+
+func _on_Button3_pressed():
+	check_if_correct($TextureRect/Button3/Label4)
+
+func _on_Button4_pressed():
+	check_if_correct($TextureRect/Button4/Label3)
---
+++ drawing.gd
@@ -0,0 +1,71 @@
+extends Node2D
+
+var lines = [] # Cada item é um array de pontos: [[p1, p2], [p3, p4]]
+var drawing = false
+var erasing = false
+
+const MAX_POINTS = 5000
+const ERASE_RADIUS = 50.0 # Aumentei um pouco para facilitar
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			drawing = event.pressed
+			if drawing and get_total_points() < MAX_POINTS:
+				lines.append([event.position])
+		
+		elif event.button_index == BUTTON_RIGHT:
+			erasing = event.pressed
+			if erasing:
+				erase_at_position(event.position)
+
+	elif event is InputEventMouseMotion:
+		if drawing and get_total_points() < MAX_POINTS:
+			lines[-1].append(event.position)
+			update()
+		elif erasing:
+			erase_at_position(event.position)
+			update()
+
+func erase_at_position(pos):
+	var new_lines_list = []
+	var changed = false
+
+	for line in lines:
+		var current_segment = []
+		for p in line:
+			if pos.distance_to(p) > ERASE_RADIUS:
+				current_segment.append(p)
+			else:
+				# Ponto apagado! Se tínhamos algo acumulado, vira uma linha separada
+				if current_segment.size() > 1:
+					new_lines_list.append(current_segment)
+				current_segment = [] # Reseta para começar uma nova linha após o "buraco"
+				changed = true
+		
+		# Adiciona o que sobrou da linha original
+		if current_segment.size() > 1:
+			new_lines_list.append(current_segment)
+	
+	if changed:
+		lines = new_lines_list
+		update()
+
+func get_total_points():
+	var count = 0
+	for line in lines:
+		count += line.size()
+	return count
+
+func _draw():
+	var line_color = Color(0.1, 0.5, 1.56)
+	var line_width = 20
+	
+	for line in lines:
+		if line.size() > 1:
+			# O quarto parâmetro controla o anti-aliasing (true = ligado, false = desligado)
+			draw_polyline(PoolVector2Array(line), line_color, line_width, false)
+	
+	if erasing:
+		# Também desativado aqui para o círculo da borracha
+		draw_arc(get_local_mouse_position(), ERASE_RADIUS, 0, TAU, 24, Color.yellow, 1.5, false)
